56112dc996c807d52206b670c1feee67c7e14ba6
ritiksharmarj <theonlyritik@gmail.com>
2024-07-14 22:27:47 +0530

    fix: Update ChatGPT webview URL in extension.js

--- extension.js
+++ extension.js
@@ -1,13 +1,5 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
   // Register a command that will be called when the extension icon is clicked
   const disposable = vscode.commands.registerCommand(
@@ -19,8 +11,8 @@ function activate(context) {
         'ChatGPT', // Title of the panel displayed to the user
         vscode.ViewColumn.One, // Editor column to show the new webview panel in
         {
-          enableScripts: true,
-          retainContextWhenHidden: true,
+          // enableScripts: true,
+          // retainContextWhenHidden: true,
         }
       );
 
@@ -34,18 +26,18 @@ function activate(context) {
 
 function getWebviewContent() {
   return `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>ChatGPT</title>
-      </head>
-      <body>
-          <iframe src="https://ritiksharma.me/" width="100%" height="100%" frameborder="0"></iframe>
-      </body>
-      </html>
-  `;
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>ChatGPT</title>
+  </head>
+  <body>
+      <iframe src="https://chatgpt.com/" width="100%" height="500px" frameborder="0"></iframe>
+  </body>
+  </html>
+`;
 }
 
 // This method is called when your extension is deactivated
