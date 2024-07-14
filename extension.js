1ca09657ba072c65057d3882185be5a7df6f07ee
ritiksharmarj <theonlyritik@gmail.com>
2024-07-14 17:46:23 +0530

    chore: Add openWebView command to activate ChatGPT webview

--- extension.js
+++ extension.js
@@ -9,28 +9,55 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "code-genius" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-genius" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    'code-genius.openWebView',
+    function () {
+      // The code you place here will be executed every time your command is executed
+      const panel = vscode.window.createWebviewPanel(
+        'webView',
+        'ChatGPT',
+        vscode.ViewColumn.One,
+        {
+          enableScripts: true,
+        }
+      );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('code-genius.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+      panel.webview.html = getWebviewContent();
+      // Display a message box to the user
+      // vscode.window.showInformationMessage('Hello World from Code Genius!');
+    }
+  );
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Code Genius!');
-	});
+  context.subscriptions.push(disposable);
+}
 
-	context.subscriptions.push(disposable);
+function getWebviewContent() {
+  return `
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+					<meta charset="UTF-8">
+					<meta name="viewport" content="width=device-width, initial-scale=1.0">
+					<title>ChatGPT</title>
+			</head>
+			<body>
+					<iframe src="https://chatgpt.com/" width="100%" height="100%" frameborder="0"></iframe>
+			</body>
+			</html>
+	`;
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
